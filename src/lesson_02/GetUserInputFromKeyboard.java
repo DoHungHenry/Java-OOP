496b837d0018d039a4f928ccc1256a3d42571530
unknown <brisingr2209@gmail.com>
2022-02-23 22:51:51 +0700

    Get User's Input from keyboard

---
+++ src/lesson_02/GetUserInputFromKeyboard.java
@@ -0,0 +1,15 @@
+package lesson_02;
+
+import java.util.Scanner;
+
+public class GetUserInputFromKeyboard {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int userInput = scanner.nextInt();
+
+        System.out.println("Please put your age: ");
+
+
+    }
+}
