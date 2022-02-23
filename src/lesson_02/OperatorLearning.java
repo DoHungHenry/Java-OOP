a1242e592fce5e7f14b581d14bb14db61e582254
unknown <brisingr2209@gmail.com>
2022-02-23 19:28:37 +0700

    break line with \n

--- src/lesson_02/OperatorLearning.java
+++ src/lesson_02/OperatorLearning.java
@@ -14,6 +14,10 @@ public class OperatorLearning {
         //phep chia: 5 / 2 = 2, 5 % 2 = 1
         int nguyen = 5/2;
         int remaining = 5%2;
-        System.out.printf("%d chia %d = %d du %d", 5, 2, nguyen, remaining);
+        System.out.printf("%d chia %d = %d du %d \n", 5, 2, nguyen, remaining);
+
+        //convention to bind correct type of variable
+        // %b (boolean), %s (string), %d (so nguyen), %f (floating number)
+        // print("string \n") => xuong dong voi \n
     }
 }
