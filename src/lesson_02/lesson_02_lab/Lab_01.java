bd6561b2c8b22d78660298171f19c8127ffe82dd
unknown <brisingr2209@gmail.com>
2022-02-24 23:18:36 +0700

    Lab01 - step 1 - print user's height and weight via keyboard inoput

--- src/lesson_02/lesson_02_lab/Lab_01.java
+++ src/lesson_02/lesson_02_lab/Lab_01.java
@@ -1,7 +1,15 @@
 package lesson_02.lesson_02_lab;
 
+import java.util.Scanner;
+
 public class Lab_01 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.printf("Please enter your Weight: ");
+        int weight = scanner.nextInt();
+        System.out.printf("Please enter your Height: ");
+        int height = scanner.nextInt();
+
 
     }
 }
