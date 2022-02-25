04ec9661b1cd7c99f5631037815cb94397814e47
unknown <brisingr2209@gmail.com>
2022-02-25 20:48:48 +0700

    ArrayIndexOutOfBoundsException when using <= syntax

--- src/lesson_03/BreakKeyword.java
+++ src/lesson_03/BreakKeyword.java
@@ -4,11 +4,17 @@ public class BreakKeyword {
     public static void main(String[] args) {
         int[] intArr = {2,34,13,67,34};
 
-        for (int i = 0; i < intArr.length; i++) {
-            if (intArr[i] == 34){
-                System.out.printf("the number %d is index %d of Array \n", 34, i);
-                break;
-            }
-        }
+//        for (int i = 0; i < intArr.length; i++) {
+//            if (intArr[i] == 34){
+//                System.out.printf("the number %d is index %d of Array \n", 34, i);
+//                break; // get out LOOP even still a 34 value is in array
+//            }
+//        }
+        // use <= instead of < : exception error because we access index 5
+//        for (int i = 0; i <= intArr.length; i++) {
+//            System.out.println(intArr[i]);
+//        }
+
+
     }
 }
