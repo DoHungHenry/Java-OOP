9a0def8d1144fa52dc6845365963055115a04852
unknown <brisingr2209@gmail.com>
2022-02-25 19:36:52 +0700

    Infinitive Loop example

--- src/lesson_03/ForLoop.java
+++ src/lesson_03/ForLoop.java
@@ -6,6 +6,18 @@ public class ForLoop {
         for (; count < 3; count++ ){
             System.out.println("Pushing up ..." + (count + 1));
         }
+
+
+        // Infinite Loop: happen when condition is always true
+//        for (; count < 3;){
+//            System.out.println("Pushing up ..." + (count + 1));
+//        }
+
+        // Interview: below code have any problem? => infinitive loop
+//        for (;;){
+//            System.out.println("Pushing up ..." + (count + 1));
+//        }
+
         System.out.println(count); //3
     }
 }
