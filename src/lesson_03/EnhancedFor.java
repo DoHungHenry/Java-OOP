e50e3770502f19109735b86edaad5af5a201cb1c
unknown <brisingr2209@gmail.com>
2022-02-25 20:09:22 +0700

    EnhancedFor across Array without index

---
+++ src/lesson_03/EnhancedFor.java
@@ -0,0 +1,13 @@
+package lesson_03;
+
+public class EnhancedFor {
+    public static void main(String[] args) {
+        int[] intArr = {1,2,3,4,5};
+
+
+        // For across Array without index
+        for (int i : intArr) {
+            System.out.println(i);
+        }
+    }
+}
