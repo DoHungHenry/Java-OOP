c071a2799c07c60bf37be305d6a10d08a2e8d57d
unknown <brisingr2209@gmail.com>
2022-02-27 23:45:29 +0700

    Try to prevent user to input a value that not double

--- src/lesson_02/lesson_02_lab/Lab_01.java
+++ src/lesson_02/lesson_02_lab/Lab_01.java
@@ -1,14 +1,26 @@
 package lesson_02.lesson_02_lab;
-
+import java.util.InputMismatchException;
 import java.util.Scanner;
+import java.io.*;
+
 
 public class Lab_01 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.printf("Please enter your Weight: ");
-        int weight = scanner.nextInt();
-        System.out.printf("Please enter your Height: ");
-        int height = scanner.nextInt();
+
+        try {
+            System.out.printf("Please enter your Weight: ");
+            double weight = scanner.nextDouble();
+            System.out.printf("Please enter your Height: ");
+            double height = scanner.nextDouble();
+        } catch (InputMismatchException e){
+            System.out.println("Please enter only number: ");
+        }
+
+//
+
+
+
 
 
     }
