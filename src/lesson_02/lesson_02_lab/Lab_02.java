d0814fc1c41c2bdd89f05aaa8f1fb5f5511d0e65
unknown <brisingr2209@gmail.com>
2022-02-28 22:33:26 +0700

    Lesson 02 - lab 02 (missing user input validation: isInt?)

--- src/lesson_02/lesson_02_lab/Lab_02.java
+++ src/lesson_02/lesson_02_lab/Lab_02.java
@@ -1,7 +1,18 @@
 package lesson_02.lesson_02_lab;
 
+import java.util.Scanner;
+
 public class Lab_02 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.printf("Please enter a number: ");
+        int myInt = scanner.nextInt();
+
+        if ((myInt % 2) == 0) {
+            System.out.printf("Your number is an Even number");
+        } else {
+            System.out.printf("Your number is an Odd number");
+        }
 
     }
 }
