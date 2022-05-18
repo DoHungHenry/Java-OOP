7aa7b293be46447e025bd9d8791697340a10af07
HungDo <hung.do@workbuddy.com>
2022-05-18 22:27:36 +0700

    lab_01 try to commit

--- src/Java/example.java
+++ src/Java/example.java
@@ -3,5 +3,6 @@ package Java;
 public class example {
     public static void main(String[] args) {
         System.out.println("try to push new file on git after delete branch");
+        System.out.println("try to commit new one");
     }
 }
