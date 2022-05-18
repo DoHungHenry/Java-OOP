1445fa56cb4ed5f5718f857a178f799a7774efdd
DoHungHenry <brisingr2209@gmail.com>
2022-05-18 22:44:19 +0700

    try to commit with global git user name & email being set

--- src/Java/example.java
+++ src/Java/example.java
@@ -4,5 +4,6 @@ public class example {
     public static void main(String[] args) {
         System.out.println("try to push new file on git after delete branch");
         System.out.println("try to commit new one");
+        System.out.println("try to commit 2nd because contribution not retrieve");
     }
 }
