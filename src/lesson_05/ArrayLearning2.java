9bbc2189d06a163b0b6538c3f6b1acb089aa79ac
DoHungHenry <brisingr2209@gmail.com>
2022-06-09 00:10:46 +0700

    Declare HashSet with un-duplicated value, then change it to a List

--- src/lesson_05/ArrayLearning2.java
+++ src/lesson_05/ArrayLearning2.java
@@ -1,7 +1,11 @@
 package lesson_05;
 
+import com.sun.source.doctree.SeeTree;
+
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class ArrayLearning2 {
 
@@ -17,12 +21,18 @@ public class ArrayLearning2 {
 
         List<Integer> arrWithoutDuplicatedValue = new ArrayList<>();
         for (Integer i : myArrList) {
-            if (arrWithoutDuplicatedValue.contains(i)){
+            if (!arrWithoutDuplicatedValue.contains(i)){
                 arrWithoutDuplicatedValue.add(i);
             }
         }
 
         System.out.println(myArrList);
         System.out.println(arrWithoutDuplicatedValue);
+
+        Set<Integer> set = new HashSet<>(myArrList);
+        System.out.println(set);
+
+        List<Integer> changeSetToList = new ArrayList<>(set);
+        System.out.println(changeSetToList);
     }
 }
--- src/lesson_07/TestPerson.java
+++ src/lesson_07/TestPerson.java
@@ -4,6 +4,6 @@ public class TestPerson {
 
     public static void main(String[] args) {
 
-        Person teo = new Person("Teo", 18);
+//        Person teo = new Person("Teo", 18);
     }
 }
