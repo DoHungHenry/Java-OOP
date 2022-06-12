ec2f292e5c02d32f5c717bcb2fe2c680cd17ea3b
DoHungHenry <brisingr2209@gmail.com>
2022-06-12 13:16:00 +0700

    Lesson 6 - lab 01 - calculate total minutes in a string

---
+++ src/lesson_06/less_06_lab/Lesson06_lab01.java
@@ -0,0 +1,14 @@
+package lesson_06.less_06_lab;
+
+public class Lesson06_lab01 {
+    public static void main(String[] args) {
+
+        String duration = "2hrs and 5 minutes";
+
+        int hours = Integer.parseInt(duration.substring(0, 1));
+        int minutes = Integer.parseInt(duration.substring(9, 10));
+
+        int totalMinutes = hours * 60 + minutes;
+        System.out.println("Total minutes of '2hrs and 5 minutes' string is equal " + totalMinutes + " minute(s)");
+    }
+}
