c728a73973043ec8e1faaa96a5bab631ca720aa8
DoHungHenry <brisingr2209@gmail.com>
2022-06-16 08:21:02 +0700

    Static member, class variable, class method usage

--- src/lesson_08/RobotDog.java
+++ src/lesson_08/RobotDog.java
@@ -3,12 +3,17 @@ package lesson_08;
 public class RobotDog {
 
     public static int dogId = 0;
+    public String dogName = "Banh Mi";
 
     public void increaseDogId(){
         dogId++;
     }
 
-    public int getDogId() {
+    public static int getDogId() {
+        // dogName can not be referred in this method because it's non-static
+        // return dogName;
+
+        // dogId can be referred in this method because it's static
         return dogId;
     }
 }
