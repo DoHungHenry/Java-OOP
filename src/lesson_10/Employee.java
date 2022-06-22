507422daf47deac1397d780394a1e932d2107bc5
DoHungHenry <brisingr2209@gmail.com>
2022-06-22 23:49:11 +0700

    Abstraction

---
+++ src/lesson_10/Employee.java
@@ -0,0 +1,13 @@
+package lesson_10;
+
+public abstract class Employee {
+
+    private String name;
+    private int age;
+
+    public Employee() {
+    }
+
+    public abstract String getName();
+    public abstract int getAge();
+}
---
+++ src/lesson_10/FTE.java
@@ -0,0 +1,14 @@
+package lesson_10;
+
+public class FTE extends Employee {
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public int getAge() {
+        return 0;
+    }
+}
